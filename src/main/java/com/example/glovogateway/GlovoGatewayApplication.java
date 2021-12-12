b11339e98956abd390353fd842ab3fd408119d35
nainia-youness <nainiayouness@gmail.com>
2021-12-12 20:21:25 +0100

    first commit

---
+++ src/main/java/com/example/glovogateway/GlovoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.glovogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlovoGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlovoGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/glovogateway/GlovoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glovogateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlovoGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
